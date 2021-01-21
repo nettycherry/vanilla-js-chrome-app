4746f64f797ae7d7f30e6ab745ea8b64cfc2d411
nettycherry <nettycherry@users.noreply.github.com>
2021-01-21 22:35:18 +0900

    save to-do-list in the local storage

--- todo.js
+++ todo.js
@@ -4,19 +4,51 @@ const toDoForm=document.querySelector(".js-toDoForm"),
 
 const TODOS_LS="toDos";
 
+const toDos = []; 
+//비어 있는 목록, to do 저장할 때마다 이 array에 저장되도록
+
+function deleteToDo(event){
+    console.log(event);
+
+}
+
+function saveToDo(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos))
+    //LS는 string만 저장하기 때문에 javascript의 모든 object를 string으로 바꿔주는 JSON.stringify 이용
+}
+
 
 function paintToDo(text){
     const li = document.createElement("li");
+
     const delBtn=document.createElement("button");
     delBtn.innerText="❌";
     //이모지 -> 윈도우 : 윈도우키+.또는; / 맥OS : 커맨드+컨트롤+스페이스
+    delBtn.addEventListener("click",deleteToDo);
+    
     const span=document.createElement("span");
     span.innerText=text;
     //submit function에서 온 텍스트값
+    
+    const newId = toDos.length +1;
+    //원래 배열은 0부터 시작이지만, id는 1부터 시작할 수 있게
+    li.id=newId; //li에 id 부여
+
     li.appendChild(span);
     li.appendChild(delBtn); 
     toDoList.appendChild(li);
-    //appendChild(): puts something inside of its father
+    /*appendChild(): puts something inside of its father
+    li에 span, delBtn 넣고, ul에 li 넣고*/
+    
+    const toDoObj = {
+        text: text, 
+        //text라는 키에 paintToDo(text)의 text가 옴
+        id:newId 
+    };
+    
+    toDos.push(toDoObj); 
+    //toDos array에 toDoObj 넣어줌
+    saveToDo(); //push 다음에 호출해야 함
 }
 
 function handleSubmit(event){
@@ -29,8 +61,12 @@ function handleSubmit(event){
 }
 
 function loadToDos(){
-    const toDos=localStorage.getItem(TODOS_LS);
+    const loadedToDos=localStorage.getItem(TODOS_LS);
     if(toDos!==null){
+        console.log(loadedToDos);
+        const parsedToDos = JSON.parse(loadedToDos);
+        //string으로 저장된 것을 object로 바꿔줌
+        console.log(parsedToDos);
 
     } 
 }
