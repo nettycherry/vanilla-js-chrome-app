c958376526d808cb262f85356eca4040912a9881
nettycherry <nettycherry@users.noreply.github.com>
2021-01-22 02:21:10 +0900

    find and save geolocation

---
+++ weather.js
@@ -0,0 +1,42 @@
+const COORDS = 'coords'
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS,JSON.stringify(coordsObj))
+}
+
+function handleGeoSuccess(position){
+    const latitude=position.coords.latitude;
+    const longitude=position.coords.longitude;
+    const coordsObj={
+        latitude:latitude,
+        longitude:longitude
+        // 이름 똑같으니까 :이하 생략 가능
+    };
+    saveCoords(coordsObj);
+}
+
+function handleGeoError(){
+    console.log('Cant access geo location');
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess,handleGeoError)
+
+}
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS)
+    if(loadedCoords===null){
+        askForCoords();
+    } else {
+        //getWeather
+    }
+
+}
+
+
+function init(){
+    loadCoords();
+}
+
+init();
